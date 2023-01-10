ea57f967cd311fb68ac6ee4185e0dd6fda8a2c42
abbysheridan7 <115241274+abbysheridan7@users.noreply.github.com>
2023-01-10 17:51:25 -0500

    Add files via upload

---
+++ Unit 1/Project/First App/Shared/First_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_AppApp.swift
+//  Shared
+//
+//  Created by Abigail Sheridan on 1/7/23.
+//
+
+import SwiftUI
+
+@main
+struct First_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
